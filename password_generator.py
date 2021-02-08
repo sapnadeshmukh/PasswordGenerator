106f7222b76fc3909572e117aec8fc3351305eec
sapnadeshmukh <sapnad18@navgurukul.org>
2021-02-08 14:25:05 +0530

    password file

---
+++ password_generator.py
@@ -0,0 +1,13 @@
+import random
+def passwordGenerator():
+    lowerchars=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    upperchars=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    specialchars=['!','@','#','$','%','*','&']
+    numericchars=['0','1','2','3','4','5','6','7','8','9']
+
+    password=random.choice(lowerchars)+random.choice(upperchars)+random.choice(specialchars)+random.choice(numericchars)
+    return(password+password)
+password=passwordGenerator()
+
+print("Your new and strong password is:---",password)
+
